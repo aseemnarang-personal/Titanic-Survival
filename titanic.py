62db5810d47d8d734d595f698914eecb108a9ee2
aseemnarang17 <aseemnarang17@yahoo.com>
2016-09-21 13:24:00 +0100

    classifier using Logistic regression

---
+++ titanic.py
@@ -0,0 +1,95 @@
+#imports
+
+import pandas as pd 
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.cross_validation import KFold
+from sklearn import cross_validation
+from sklearn.linear_model import LogisticRegression  
+
+
+titanic = pd.read_csv("titanic_train.csv")
+
+#fill null values in age  witht hte median of age
+titanic["Age"] = titanic["Age"].fillna(titanic["Age"].median())
+
+
+# Replace all the occurences of male with the number 0 femal with 1
+titanic.loc[titanic["Sex"] == "male", "Sex"] = 0
+titanic.loc[titanic["Sex"] == "female", "Sex"] = 1
+
+#??
+titanic["Embarked"] = titanic["Embarked"].fillna("S")
+
+titanic.loc[titanic["Embarked"] == "S", "Embarked"] = 0
+titanic.loc[titanic["Embarked"] == "C", "Embarked"] = 1
+titanic.loc[titanic["Embarked"] == "Q", "Embarked"] = 2
+
+
+
+# usefull columns
+predictors = ["Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked"]
+
+# Initialize our algorithm class
+alg = LinearRegression()
+# Generate cross validation folds for the titanic dataset.  It return the row indices corresponding to train and test.
+# We set random_state to ensure we get the same splits every time we run this.
+kf = KFold(titanic.shape[0], n_folds=3, random_state=1)
+
+predictions = []
+for train, test in kf:
+    train_predictors = (titanic[predictors].iloc[train,:])
+    train_target = titanic["Survived"].iloc[train]
+    alg.fit(train_predictors, train_target)
+    test_predictions = alg.predict(titanic[predictors].iloc[test,:])
+    predictions.append(test_predictions)
+
+
+
+# The predictions are in three separate numpy arrays.  Concatenate them into one.  
+# We concatenate them on axis 0, as they only have one axis.
+predictions = np.concatenate(predictions, axis=0)
+
+# Map predictions to outcomes (only possible outcomes are 1 and 0)
+predictions[predictions > .5] = 1
+predictions[predictions <=.5] = 0
+accuracy = sum(predictions[predictions == titanic["Survived"]]) / len(predictions)
+
+
+# Initialize our algorithm
+alg = LogisticRegression(random_state=1)
+# Compute the accuracy score for all the cross validation folds.  (much simpler than what we did before!)
+scores = cross_validation.cross_val_score(alg, titanic[predictors], titanic["Survived"], cv=3)
+# Take the mean of the scores (because we have one for each fold)
+print(scores.mean())
+
+#do the same pre processing to the
+titanic_test = pd.read_csv("titanic_test.csv")
+titanic_test["Age"] = titanic_test["Age"].fillna(titanic["Age"].median())
+titanic_test["Fare"] = titanic_test["Fare"].fillna(titanic_test["Fare"].median())
+titanic_test.loc[titanic_test["Sex"] == "male", "Sex"] = 0 
+titanic_test.loc[titanic_test["Sex"] == "female", "Sex"] = 1
+titanic_test["Embarked"] = titanic_test["Embarked"].fillna("S")
+
+titanic_test.loc[titanic_test["Embarked"] == "S", "Embarked"] = 0
+titanic_test.loc[titanic_test["Embarked"] == "C", "Embarked"] = 1
+titanic_test.loc[titanic_test["Embarked"] == "Q", "Embarked"] = 2
+
+
+
+# Initialize the algorithm class
+alg = LogisticRegression(random_state=1)
+
+# Train the algorithm using all the training data
+alg.fit(titanic[predictors], titanic["Survived"])
+
+# Make predictions using the test set.
+predictions = alg.predict(titanic_test[predictors])
+
+print (len(predictions))
+'''
+# Create a new dataframe with only the columns Kaggle wants from the dataset.
+submission = pd.DataFrame({
+        "PassengerId": titanic_test["PassengerId"],
+        "Survived": predictions
+    })'''
