aadc9adea386dd721a031c7ec98783655ca3cdd2
aseemnarang17 <aseemnarang17@yahoo.com>
2016-09-26 18:18:27 +0100

    Random Forrest implements

--- titanic.py
+++ titanic.py
@@ -4,8 +4,11 @@ import pandas as pd
 import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.cross_validation import KFold
-from sklearn import cross_validation
-from sklearn.linear_model import LogisticRegression  
+from sklearn import cross_validation as cv
+from sklearn.linear_model import LogisticRegression
+
+import warnings
+warnings.filterwarnings("ignore")
 
 
 titanic = pd.read_csv("titanic_train.csv")
@@ -26,7 +29,6 @@ titanic.loc[titanic["Embarked"] == "C", "Embarked"] = 1
 titanic.loc[titanic["Embarked"] == "Q", "Embarked"] = 2
 
 
-
 # usefull columns
 predictors = ["Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked"]
 
@@ -34,7 +36,7 @@ predictors = ["Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked"]
 alg = LinearRegression()
 # Generate cross validation folds for the titanic dataset.  It return the row indices corresponding to train and test.
 # We set random_state to ensure we get the same splits every time we run this.
-kf = KFold(titanic.shape[0], n_folds=3, random_state=1)
+kf = KFold(titanic.shape[0], n_folds=5, random_state=1)
 
 predictions = []
 for train, test in kf:
@@ -59,9 +61,71 @@ accuracy = sum(predictions[predictions == titanic["Survived"]]) / len(prediction
 # Initialize our algorithm
 alg = LogisticRegression(random_state=1)
 # Compute the accuracy score for all the cross validation folds.  (much simpler than what we did before!)
-scores = cross_validation.cross_val_score(alg, titanic[predictors], titanic["Survived"], cv=3)
+logreg_scores = cv.cross_val_score(alg, titanic[predictors], titanic["Survived"], cv=3)
 # Take the mean of the scores (because we have one for each fold)
-print(scores.mean())
+
+print('logistic regression score:  ' + str(logreg_scores.mean()))
+
+
+### LOGISTIC REGRESSION END ####
+
+###### RANDOM FORREST #####
+
+
+'''
+## feature generariton
+
+# Generating a familysize column
+titanic["FamilySize"] = titanic["SibSp"] + titanic["Parch"]
+
+# The .apply method generates a new series
+titanic["NameLength"] = titanic["Name"].apply(lambda x: len(x))
+
+##
+'''
+
+
+from sklearn.ensemble import RandomForestClassifier
+
+predictors = ["Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked", "FamilySize" , "NameLength"]
+
+# Initialize our algorithm with the default paramters
+# n_estimators is the number of trees we want to make
+# min_samples_split is the minimum number of rows we need to make a split
+# min_samples_leaf is the minimum number of samples we can have at the place where a tree branch ends (the bottom points of the tree)
+alg = RandomForestClassifier(random_state=1, n_estimators=50, min_samples_split=4, min_samples_leaf=2)
+kf = cv.KFold(titanic.shape[0], n_folds=3, random_state=1)
+
+predictions = []
+for train, test in kf:
+    train_predictors = (titanic[predictors].iloc[train,:])
+    train_target = titanic["Survived"].iloc[train]
+    alg.fit(train_predictors, train_target)
+    test_predictions = alg.predict(titanic[predictors].iloc[test,:])
+    predictions.append(test_predictions)
+
+# The predictions are in three separate numpy arrays.  Concatenate them into one.
+# We concatenate them on axis 0, as they only have one axis.
+predictions = np.concatenate(predictions, axis=0)
+
+# Map predictions to outcomes (only possible outcomes are 1 and 0)
+predictions[predictions > .5] = 1
+predictions[predictions <=.5] = 0
+
+accuracy = sum(predictions[predictions == titanic["Survived"]]) / len(predictions)
+#print(accuracy)
+
+rf_scores = cv.cross_val_score(alg, titanic[predictors], titanic["Survived"], cv=3)
+print ('Random Forrest score:  ' + str(rf_scores.mean()))
+
+
+
+
+
+
+
+
+
 
 #do the same pre processing to the
 titanic_test = pd.read_csv("titanic_test.csv")
